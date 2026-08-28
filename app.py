a8d8f83091a604ae272e427da7479d58e4cb951f
chaitanyakhaire15-collab <chaitanyakhaire.15@gmail.com>
2026-08-28 23:34:18 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3, os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='tamplates')
 
 # --- CONFIG - EDIT HERE ---
 UPI_ID = "yourupi@paytm"
